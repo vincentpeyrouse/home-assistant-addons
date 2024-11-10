1d1ec6a76cecc249d84eb48ec059981317011ee8
Vincent PEYROUSE <vincent.peyrouse@ecole-hexagone.com>
2024-11-10 23:27:56 +0100

    Add ingress support and use dynamic port

--- hamh/docker-entrypoint.sh
+++ hamh/docker-entrypoint.sh
@@ -4,6 +4,6 @@ home-assistant-matter-hub start \
   --log-level=$(bashio::config 'app_log_level') \
   --disable-log-colors=$(bashio::config 'disable_log_colors') \
   --storage-location=/config/data \
-  --web-port=$(bashio::config 'web_port') \
+  --web-port="$(bashio::addon.ingress_port)" \
   --home-assistant-url='http://supervisor/core' \
   --home-assistant-access-token="$SUPERVISOR_TOKEN"
